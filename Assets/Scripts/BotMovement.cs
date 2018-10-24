df539117b212580e7238fc5dcb577202d8e13dad
unknown <Triplett.Seth@gmail.com>
2018-10-24 00:01:32 -0400

    Add basic bot model, animations, and AI.

---
+++ Assets/Scripts/BotMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BotMovement: MonoBehaviour {
+
+    private enum BotMode {
+        Follow, Idle
+    }
+
+    [SerializeField] private BotMode botMode;
+    [SerializeField] private Transform followTarget;
+    private Animator anim;
+    private NavMeshAgent navMeshAgent;
+    
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        if (botMode == null) {
+            botMode = BotMode.Follow;
+        }
+    }
+
+    void Update() {
+        if (botMode == BotMode.Follow) {
+            if (navMeshAgent.destination.x != followTarget.position.x || navMeshAgent.destination.z != followTarget.position.z) {
+                print(navMeshAgent.destination);
+                print(flattenTransform(followTarget));
+                navMeshAgent.SetDestination(flattenTransform(followTarget));
+                anim.SetBool("IsBlock", false);
+                anim.SetBool("Walking", true);
+            } else if (navMeshAgent.remainingDistance <= 0.9f) {
+                anim.SetBool("IsBlock", true);
+            } else {
+                anim.SetBool("Walking", true);
+            }
+        }
+    }
+
+    // Takes a transform and turns it into a vector3 with y of 0
+    private Vector3 flattenTransform(Transform transform) {
+        return new Vector3(transform.position.x, 0, transform.position.z);
+    }
+
+}
