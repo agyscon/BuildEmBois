0e6def8e0e77d789a522b0ecf9ee73b58978a367
agyscon 67 <agysconner67@gmail.com>
2018-10-25 14:31:20 -0400

    Main Menu Things

---
+++ Assets/Scripts/MainMenuButtonHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    // Use this for initialization
+    public Text buttonText;
+    private string originalString;
+	void Start () {
+        //buttonText = this.GetComponent<Text>();		
+        originalString = buttonText.text;
+	}
+ 
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.text = ">" + buttonText.text + "<";
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.text = originalString;
+    }
+}
