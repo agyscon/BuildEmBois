80b1af869bbdbdc28cc72a08e3a53bd186d4d70b
agyscon 67 <agysconner67@gmail.com>
2018-10-29 21:35:11 -0400

    Build Pads Added

---
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformScript : MonoBehaviour {
+    int playerMask;
+
+    private void Start()
+    {
+        playerMask = LayerMask.NameToLayer("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == playerMask)
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
