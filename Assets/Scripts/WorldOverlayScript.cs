bd76a15391e2dca46d48699394a342046f4fa04a
agyscon 67 <agysconner67@gmail.com>
2018-10-29 22:30:46 -0400

    Overlay Added

---
+++ Assets/Scripts/WorldOverlayScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldOverlayScript : MonoBehaviour {
+    private Text numBotsText;
+	// Use this for initialization
+	void Start () {
+        numBotsText = GetComponentInChildren<Text>();
+	}
+
+    void setNumBots(int n)
+    {
+        numBotsText.text = "x" + n;
+    }
+
+
+}
