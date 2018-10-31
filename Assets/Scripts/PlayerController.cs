30701ef496923c030beb6fc0b67543267102a8bc
unknown <Triplett.Seth@gmail.com>
2018-10-30 23:24:57 -0400

    Added placeholder engineer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public Vector3 jump;
     public float jumpForce = 5.0f;
-
+    private bool canJump;
     public bool isGrounded;
     Rigidbody rb;
     Animator anim;
@@ -24,19 +24,25 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = true;
         }
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        //anim.SetFloat("velx", x);
+        anim.SetFloat("Speed", z);
+        
+        x = x * Time.deltaTime * 150.0f;
+        z = z * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        //transform.Translate(0, 0, z);
 
         if (Input.GetKeyDown ("space") && isGrounded)
         {
             // add impulse to move the character up and fall again
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+            anim.SetTrigger("Jump");
         }
-
 
     }
 }
