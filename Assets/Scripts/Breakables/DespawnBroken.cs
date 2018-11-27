2cafad769465c72ea74eaa403b54d35c571712ca
agyscon 67 <agysconner67@gmail.com>
2018-11-26 21:24:26 -0500

    breakable logic

---
+++ Assets/Scripts/Breakables/DespawnBroken.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnBroken : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(waitForSeconds());
+		
+	}
+
+    IEnumerator waitForSeconds()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(transform.gameObject);
+    }
+}
