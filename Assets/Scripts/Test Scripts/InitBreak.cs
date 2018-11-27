fcbbdf598a249cbb93e128347363c04efbc16877
agyscon 67 <agysconner67@gmail.com>
2018-11-26 21:01:13 -0500

    explosion logic

---
+++ Assets/Scripts/Test Scripts/InitBreak.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitBreak : MonoBehaviour {
+
+    public GameObject remains;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	}
+
+    public GameObject breakObject()
+    {
+        GameObject newObject = Instantiate(remains, transform.position, transform.rotation);
+        Destroy(transform.gameObject);
+        return newObject;
+    }
+}
