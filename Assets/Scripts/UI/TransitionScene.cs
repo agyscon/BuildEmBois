6e70c03d5a57f12735c7a81c90dc314a7922ce94
agyscon 67 <agysconner67@gmail.com>
2018-12-11 22:14:03 -0500

    Added Scene Transition

---
+++ Assets/Scripts/UI/TransitionScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TransitionScene : MonoBehaviour {
+
+    public Text loadingText;
+    public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(waitForSeconds());
+        StartCoroutine(waitForOneSecond());
+
+    }
+
+    IEnumerator waitForSeconds()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(nextScene);
+    }
+
+    IEnumerator waitForOneSecond()
+    {
+        yield return new WaitForSeconds(1.5f);
+        loadingText.text = loadingText.text + ".";
+    }
+}
